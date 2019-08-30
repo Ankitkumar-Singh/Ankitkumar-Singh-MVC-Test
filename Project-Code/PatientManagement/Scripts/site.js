77abb12dcde622f63da135d24b899264392678ff
AnkitKumar  Singh <52848024+Anki7s@users.noreply.github.com>
2019-08-30 14:24:05 +0530

    bug are fixed

--- Project-Code/PatientManagement/Scripts/site.js
+++ Project-Code/PatientManagement/Scripts/site.js
@@ -1,8 +1,8 @@
 ﻿//Function to validate message from.
 function onchangeevent() {
     $("#noticeMsg").hide();
-    var msgvalue = $('#doctorMsg').val();
-    if (msgvalue == '') {
+    var msgvalue = jQuery.trim($("#doctorMsg").val());
+    if ((msgvalue.length == 0)) {
         $("#btnSubmit").attr("disabled", true);
         document.getElementById("errorMsg").innerHTML = "Please enter your message.";
     }
@@ -12,3 +12,23 @@ function onchangeevent() {
 
     }
 }
+
+//Function to validate Appointment date from.
+
+//Function to validate message from.
+function CheckForm() {
+    var treatment = jQuery.trim($("#treatment").val());
+    var treatmentBill = jQuery.trim($("#treatmentBill").val());
+
+    if ((treatment.length != 0 && treatmentBill.length != 0)) {
+        $('#btnSubmit').attr("disabled", false);
+        document.getElementById("errorMsg").innerHTML = " ";
+    }
+    else {
+        $("#btnSubmit").attr("disabled", true);
+        document.getElementById("errorMsg").innerHTML = "Please enter your message.";
+
+
+    }
+}
+
